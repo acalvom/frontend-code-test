34e4f87fc6f20f8a1a07ff398d13d0b6a1aa6ce9
acalvom <andreac.3108@gmail.com>
2024-10-30 12:15:22 +0100

    refactor: convert component into arrow function

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import { observer } from 'mobx-react'
-import BoxDraggable from './BoxDraggable'
+import { BoxDraggable } from './BoxDraggable'
 import './Box.css'
 
-function Box(props) {
+export const Box = observer((props) => {
   return (
     <BoxDraggable {...props}>
       <div>Box</div>
     </BoxDraggable>
   )
-}
-
-export default observer(Box)
+})
