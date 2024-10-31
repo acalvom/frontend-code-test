007b06310ae021ccd5a5f8438ad9a558002a6149
acalvom <andreac.3108@gmail.com>
2024-10-31 09:26:10 +0100

    test: add tests to remove boxed & add store setup

--- src/pages/Home/Home.test.js
+++ src/pages/Home/Home.test.js
@@ -2,20 +2,39 @@ import React from 'react'
 import { Home } from './Home'
 import '@testing-library/jest-dom'
 
-import { render, screen } from '@testing-library/react'
+import { cleanup, render, screen, wait } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
+import { Canvas, Toolbar } from '../../components'
+import { storeSetup } from '../../test/storeSetup'
+
+function customRender(ui, { initialState, ...renderOptions } = {}) {
+  const store = storeSetup()
+
+  function Wrapper() {
+    return (
+      <>
+        <Toolbar store={store} />
+        <Canvas store={store} />
+      </>
+    )
+  }
+
+  return render(ui, { wrapper: Wrapper, ...renderOptions })
+}
 
 describe('Home', () => {
+  afterEach(() => cleanup())
+
   it('should display a single box', () => {
-    render(<Home />)
+    customRender(<Home />)
 
     const box = screen.getByText('Box 1')
     expect(box).toBeInTheDocument()
   })
 
   it('should add a box in the canvas', () => {
-    render(<Home />)
+    customRender(<Home />)
 
     const canvas = screen.getByLabelText('canvas')
     expect(canvas).toBeInTheDocument()
@@ -29,5 +48,109 @@ describe('Home', () => {
 
     const box = screen.getByText('Box 2')
     expect(box).toBeInTheDocument()
+  })
+
+  it('should remove a box in the canvas', () => {
+    customRender(<Home />)
+
+    const canvas = screen.getByLabelText('canvas')
+    expect(canvas).toBeInTheDocument()
+
+    userEvent.click(canvas)
+
+    const addBoxButton = screen.getByText('Add Box')
+    expect(addBoxButton).toBeInTheDocument()
+
+    userEvent.click(addBoxButton)
+
+    const box = screen.getByText('Box 2')
+    expect(box).toBeInTheDocument()
+
+    userEvent.click(box)
+
+    const removeBoxButton = screen.getByText('Remove Box')
+    expect(removeBoxButton).toBeInTheDocument()
+
+    userEvent.click(removeBoxButton)
+
+    expect(screen.queryByText('Box 2')).not.toBeInTheDocument()
+  })
+
+  it('should remove two boxes in the canvas', () => {
+    customRender(<Home />)
+
+    const canvas = screen.getByLabelText('canvas')
+    expect(canvas).toBeInTheDocument()
+
+    const box1 = screen.getByText('Box 1')
+    expect(box1).toBeInTheDocument()
+
+    userEvent.click(canvas)
+
+    const addBoxButton = screen.getByText('Add Box')
+    expect(addBoxButton).toBeInTheDocument()
+
+    userEvent.click(addBoxButton)
+
+    const box2 = screen.getByText('Box 2')
+    expect(box2).toBeInTheDocument()
+
+    const removeBoxButton = screen.getByText('Remove Box')
+    expect(removeBoxButton).toBeInTheDocument()
+
+    userEvent.click(box1)
+    userEvent.click(removeBoxButton)
+
+    expect(screen.queryByText('Box 1')).not.toBeInTheDocument()
+
+    userEvent.click(screen.getByText('Box 2'))
+    userEvent.click(removeBoxButton)
+
+    expect(screen.queryByText('Box 2')).not.toBeInTheDocument()
+  })
+
+  it('should display a message when no box is selected', async () => {
+    customRender(<Home />)
+
+    const canvas = screen.getByLabelText('canvas')
+    expect(canvas).toBeInTheDocument()
+
+    userEvent.click(canvas)
+
+    const addBoxButton = screen.getByText('Add Box')
+    expect(addBoxButton).toBeInTheDocument()
+
+    userEvent.click(addBoxButton)
+
+    const removeBoxButton = screen.getByText('Remove Box')
+    expect(removeBoxButton).toBeInTheDocument()
+
+    userEvent.click(removeBoxButton)
+
+    const toast = await screen.findByText('No box is selected')
+    expect(toast).toBeInTheDocument()
+  })
+
+  it('should display a message when there are no boxes to remove', async () => {
+    customRender(<Home />)
+
+    const canvas = screen.getByLabelText('canvas')
+    expect(canvas).toBeInTheDocument()
+
+    const box1 = screen.getByText('Box 1')
+    expect(box1).toBeInTheDocument()
+
+    const removeBoxButton = screen.getByText('Remove Box')
+    expect(removeBoxButton).toBeInTheDocument()
+
+    userEvent.click(box1)
+    userEvent.click(removeBoxButton)
+
+    expect(screen.queryByText('Box 1')).not.toBeInTheDocument()
+
+    userEvent.click(removeBoxButton)
+
+    const toast = await screen.findByText('There are no boxes to remove')
+    expect(toast).toBeInTheDocument()
   })
 })
