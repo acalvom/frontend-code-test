28bca6cf8947b9c835a7eb74dae48170e2dcd391
acalvom <andreac.3108@gmail.com>
2024-11-01 14:34:00 +0100

    refactor: arrange code, apply srp, and separate concerns

---
+++ src/stores/actions/MainStoreActions.js
@@ -0,0 +1,63 @@
+import { getSnapshot, onAction } from 'mobx-state-tree'
+import { createBox } from './BoxActions'
+
+const MainStoreKey = 'MainStore'
+
+export const MainStoreActions = (self) => {
+  const addBox = (name, left, top) => {
+    const newBox = createBox({ name, left, top })
+    self.boxes.push(newBox)
+  }
+
+  const removeBox = (box) => {
+    if (self.selectedBox === box) clearSelection()
+    self.boxes.remove(box)
+  }
+
+  const selectBox = (box) => {
+    self.selectedBox = box
+  }
+
+  const clearSelection = () => {
+    self.selectedBox = null
+  }
+
+  const saveToLocalStorage = () => {
+    const currentState = getSnapshot(self)
+    localStorage.setItem(MainStoreKey, JSON.stringify(currentState))
+  }
+
+  const loadFromLocalStorage = () => {
+    const savedState = localStorage.getItem(MainStoreKey)
+    if (savedState) {
+      const parsedState = JSON.parse(savedState)
+      self.boxes.replace(parsedState.boxes)
+      self.cursorPosition = parsedState.cursorPosition
+      self.selectedBox = parsedState.selectedBox
+    }
+  }
+
+  const initializeStore = () => {
+    loadFromLocalStorage()
+    if (self.boxes.length === 0) {
+      addBox('Box 1', 0, 0)
+    }
+  }
+
+  const setupActionListener = () => {
+    onAction(self, (call) => {
+      if (call.type !== 'saveToLocalStorage') {
+        saveToLocalStorage()
+      }
+    })
+  }
+
+  return {
+    addBox,
+    removeBox,
+    selectBox,
+    clearSelection,
+    initializeStore,
+    setupActionListener
+  }
+}
--- src/stores/models/MainStore.js
+++ src/stores/models/MainStore.js
@@ -1,7 +1,7 @@
-import { onAction, types } from 'mobx-state-tree'
+import { types } from 'mobx-state-tree'
 import { CursorPointerModel } from './CursorPointer'
 import { BoxModel } from './Box'
-import { createBox } from '../actions/BoxActions'
+import { MainStoreActions } from '../actions/MainStoreActions'
 
 const MainStore = types
   .model('MainStore', {
@@ -9,43 +9,18 @@ const MainStore = types
     cursorPosition: types.optional(CursorPointerModel, { x: 0, y: 0 }),
     selectedBox: types.maybeNull(types.reference(BoxModel)),
   })
-  .actions((self) => ({
-    addBox(name, left, top) {
-      const newBox = createBox({ name, left, top })
-      self.boxes.push(newBox)
-    },
-    removeBox(box) {
-      if (self.selectedBox === box) this.clearSelection()
-      self.boxes.remove(box)
-    },
-    selectBox(box) {
-      self.selectedBox = box
-    },
-    clearSelection() {
-      self.selectedBox = null
-    },
-    saveToLocalStorage() {
-      localStorage.setItem('mainStore', JSON.stringify(self))
-    },
-  }))
-  .actions((self) => ({
-    afterCreate() {
-      const savedState = localStorage.getItem('mainStore')
-      if (!savedState) {
-        self.addBox('Box 1', 0, 0)
-      } else {
-        const parsedState = JSON.parse(savedState)
-        self.boxes.replace(parsedState.boxes)
-        self.cursorPosition = parsedState.cursorPosition
-        self.selectedBox = parsedState.selectedBox
-      }
 
-      onAction(self, (call) => {
-        if (call.type !== 'saveToLocalStorage') {
-          self.saveToLocalStorage()
-        }
-      })
-    },
-  }))
+  .actions((self) => {
+    const actions = MainStoreActions(self)
+    const { setupActionListener, initializeStore } = actions
+
+    return {
+      ...actions,
+      afterCreate() {
+        initializeStore()
+        setupActionListener()
+      },
+    }
+  })
 
 export const createMainStore = () => MainStore.create()
